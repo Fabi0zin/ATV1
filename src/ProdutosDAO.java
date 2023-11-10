303c03c5d71080ca38714525fdf47d800ff8f2af
Fabio Miguel <fabiomnascimento05@gmail.com>
2023-11-10 00:47:18 -0300

    Cadastro na tabela produtos

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -12,31 +12,36 @@ import java.sql.PreparedStatement;
 import java.sql.Connection;
 import javax.swing.JOptionPane;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
-
 public class ProdutosDAO {
-    
+
     Connection conn;
     PreparedStatement prep;
     ResultSet resultset;
     ArrayList<ProdutosDTO> listagem = new ArrayList<>();
-    
-    public void cadastrarProduto (ProdutosDTO produto){
-        
-        
-        conn = new conectaDAO().connectDB();
-        
-        
+
+    public void cadastrarProduto(ProdutosDTO produto) {
+        try {
+            conn = new conectaDAO().connectDB();
+            PreparedStatement stmt = conn
+                    .prepareStatement("INSERT INTO produtos (nome, valor, status) VALUES (?, ?, ?)");
+
+            stmt.setString(1, produto.getNome());
+            stmt.setInt(2, produto.getValor());
+            stmt.setString(3, produto.getStatus());
+
+            stmt.executeUpdate();
+            conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
-    
-    public ArrayList<ProdutosDTO> listarProdutos(){
-        
+
+    public ArrayList<ProdutosDTO> listarProdutos() {
+
         return listagem;
     }
-    
-    
-    
-        
-}
 
+}
